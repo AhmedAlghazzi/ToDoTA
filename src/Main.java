7b74650134bb9279ba2e53c1d7743ef3219385c2
thair Shalal <thairshalal35@gmail.com>
2017-11-01 10:58:52 +0100

    TodoList Array Declaration

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
+import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int i=1;
-		int y = 10;
-
+		List<Todo> TodoList=new ArrayList<Todo>();
+		System.out.println("Enter Your Task Name");
+		Scanner sc= new Scanner(System.in);
+		String task=sc.nextLine();
+		System.out.println("Enter Your Task Time");
+		LocalDate date =sc.nextLine();
+		
+		
 
 
 }
