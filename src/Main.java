04a205f738683c9e09b7814cb9ba5c249d555404
thair Shalal <thairshalal35@gmail.com>
2017-11-02 09:04:45 +0100

    trying solve conflict

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,28 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-	
-
-	}
-=======
+		
+		
 		List<Todo> TodoList=new ArrayList<Todo>();
 		System.out.println("Enter Your Task Name");
 		Scanner sc= new Scanner(System.in);
 		String task=sc.nextLine();
-		System.out.println("Enter Your Task Time");
-		LocalDate date =sc.nextLine();
-		
+		System.out.println("Enter Your Task Time in year-month-day format");
+		String date =sc.nextLine();
+		System.out.println(task +" "+ date);
+		Todo T1=new Todo(task, date);
+		TodoList.add(T1);
 		
 
->>>>>>> branch 'master' of https://github.com/thair35/ToDoTA.git
+	}
+
+		
+		
 
 }
-	}
+
